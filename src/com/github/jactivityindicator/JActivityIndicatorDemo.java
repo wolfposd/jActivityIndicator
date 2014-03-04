7cc00d9ca1d02f504547999cc9b1dc29e6088619
Wolf Posdorfer <9posdorf@informatik.uni-hamburg.de>
2014-03-04 14:58:41 +0100

    adding files

---
+++ src/com/github/jactivityindicator/JActivityIndicatorDemo.java
@@ -0,0 +1,68 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2014 Mordred
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+package com.github.jactivityindicator;
+
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+/**
+ * Demo for JActivityIndicator
+ * 
+ * @author wolf.posdorfer
+ * 
+ */
+public class JActivityIndicatorDemo
+{
+    public static void main(String[] args)
+    {
+        final JActivityIndicator act = new JActivityIndicator(1);
+        final JActivityIndicator act2 = new JActivityIndicator(0);
+
+        JFrame frame = new JFrame();
+        frame.setLayout(new FlowLayout());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 300);
+
+        frame.add(act);
+        frame.add(act2);
+
+        JButton button = new JButton("Start/Stop");
+        frame.add(button);
+
+        button.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                act.toggleRotating();
+                act2.toggleRotating();
+            }
+        });
+
+        frame.setVisible(true);
+    }
+}
